ecde7ed48b7ff282b19beaa12b4ae3b5e7a979d9
Marcus Nyeholt <marcus@symbiote.com.au>
2019-01-11 14:27:19 +1100

    doc(readme)

---
+++ readme.md
@@ -0,0 +1,20 @@
+# New Relic Synthetics mock
+
+A simple wrapper around the base synthetics API
+
+## Usage
+
+* ./du.sh
+* ./dr.sh yarn install
+* ./dr.sh node exec "node sample.js" 
+
+
+## Writing tests
+
+* Copy sample.js to a new file
+* Rewrite the sample test under the `// INSERT TEST` comment
+* Run using `./dr.sh node exec "node mytest.js"`
+
+## Credits
+
+https://discuss.newrelic.com/t/local-development-of-scripts/31468/6
--- synthetics.js
+++ synthetics.js
@@ -1,9 +1,25 @@
 
 const $driver = require('selenium-webdriver');
 
-exports.$driver = $driver;
-exports.$browser = new $driver
+
+var $browser = new $driver
     .Builder()
     .forBrowser('chrome')
     .usingServer('http://selenium:4444/wd/hub')
     .build();
+
+$browser.waitForElement = function (locatorOrElement, timeoutMsOpt) {
+    return $browser.wait($driver.until.elementLocated(locatorOrElement), timeoutMsOpt || 1000, 'Timed-out waiting for element to be located using: ' + locatorOrElement);
+};
+$browser.waitForAndFindElement = function (locatorOrElement, timeoutMsOpt) {
+    return $browser.waitForElement(locatorOrElement, timeoutMsOpt)
+        .then(function (element) {
+            return $browser.wait($driver.until.elementIsVisible(element), timeoutMsOpt || 1000, 'Timed-out waiting for element to be visible using: ' + locatorOrElement)
+                .then(function () {
+                    return element;
+                });
+        });
+};
+
+exports.$driver = $driver;
+exports.$browser = $browser;
